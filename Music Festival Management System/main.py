f76d089a77db21b66a08bc20779d5a1ec79bb430
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2025-01-28 17:41:21 -0700

    Everything is working except it can't check for the new duration

--- Music Festival Management System/main.py
+++ Music Festival Management System/main.py
@@ -11,7 +11,7 @@ def ArtistManagement(Artists):
             return(Inputx)
         except:
             print("\nThat is not a valid input.")
-            return None
+            return False
 
     def AddArtist(Artists):
         #Getting Nescasary Inputs
@@ -31,7 +31,34 @@ def ArtistManagement(Artists):
             print(f"\nThe artist {ArtistToBeAdded} has been added to the list.")
 
     def EditArtist(Artists):
+        #Inputs
         name = input("\nWhat is the name of the artist that you want to edit: \t")
+        #If the artist exists
+        for x in Artists:
+            if x["Name"] == name:
+                print("\n", x)
+                choice = input("\nWhat do you want to change, 1: Name, 2: Genre, 3: Duration :\t")
+
+                if choice == "1":
+                    x["Name"] = input("\nEnter the new name: \t")
+                    print(f"\nThe new data is, {x}")
+                    mainforArtistManagement()
+                elif choice == "2":
+                    x["Genre"] = input("\nEnter the new genre: \t")
+                    print(f"\nThe new data is, {x}")
+                    mainforArtistManagement()
+                elif choice == "3":
+                    ################################################################################################################# Ask how to check if it will work
+                    Newduration = input("\nEnter the new duration: \t")
+                    print(f"\nThe new data is, {x}")
+                    mainforArtistManagement()
+                else:
+                    print("\nThat is not a valid input.")
+                    mainforArtistManagement()
+
+        #If the artist does not exists    
+        print(f"\nArtist {name} does not exist.")
+        mainforArtistManagement()
 
     def RemoveArtist(Artists):
         #Inputs
@@ -45,8 +72,6 @@ def ArtistManagement(Artists):
     
     def ViewArtists(Artists):
         print(f"\nAll of the artists are {Artists}")
-
-
 
     def mainforArtistManagement():
         choice = input("\nChoose what you want to do, 1: Add Artist, 2: Edit Artist, 3: Remove Artist, 4: View All Artists, 5: Quit : \t")
